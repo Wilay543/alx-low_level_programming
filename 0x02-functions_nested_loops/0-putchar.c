42f51151d3a5110d7382084a5cc8ac5693340df2
Wilfred Mbatia <wilfred.mbatia@325.ipa.apolloagcorp.com>
2022-07-05 23:08:36 +0300

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  * Return: always 0                                                                                                              
  */                                                                                                                              
 int main(void)                                                                                                                   
-{                                                                                                                                
+{ 
+                                                                                                                               
 _putchar('_');                                                                                                                   
 _putchar('p');                                                                                                                   
 _putchar('u');                                                                                                                   
