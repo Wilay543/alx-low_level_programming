6d83d8415f56f7dce0e8fc7b20b64b658daa7a36
Wilay543 <mbatiawilfred5@gmail.com>
2022-07-06 07:06:26 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,16 @@
  *Return: always 0                                                                                                              
  */                                                                                                                              
 int main(void)                                                                                                                   
-{ 	                                                                                                                        __putchar('_');                                                                                                                   
-_putchar('p');               
+{
+
+__putchar('_');                                                                                                                  _putchar('p');               
 _putchar('u');                                                                                                                   
 _putchar('t');                                                                                                                   
 _putchar('c');                                                                                                                   
 _putchar('h');                                                                                                                   
 _putchar('a');                                                                                                                   
 _putchar('r');                                                                                                                   
-_putchar('\n');                                                                                                                  
+_putchar('\n');
+
 return (0);  
 }    
